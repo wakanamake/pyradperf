75065c8f3e422e683c58ebd7d4a5a121b8f7d80a
wakanamake <waka.namake@gmail.com>
2022-05-27 17:45:31 +0900

    Add -sip argument and change some process to pass some parameters

--- pyradperf.py
+++ pyradperf.py
@@ -8,52 +8,90 @@ import socket
 import ipaddress
 import argparse
 
-def sendAccountingPkt(udp, pkt, n, type):
-    #print("Sending Packet#"+str(n))
-    ip = ipaddress.ip_address('10.0.0.1') + n
-    username = "hoge-" + str(n)
-    msisdn = "0123456" + str(n)
+class Config:
+    def __init__(self) -> None:
+        self.count = 10
+        self.startIp = ipaddress.ip_address('10.0.0.1')
+        self.usernameBase = "hoge-"
+        self.msisdnBase = "0123456"
+        self.server = ""
+        self.secret = six.b("")
+        self.pkt = ""
+        self.loop = False
 
-    pkt["Acct-Status-Type"]=type
-    pkt["User-Name"]=username
-    pkt["Calling-Station-Id"]=msisdn
-    pkt["Framed-IP-Address"]=str(ip)
-    pkt["Acct-Session-Id"]=str(n)
+    def setSecret(self, string):
+        self.secret = six.b(string)
+    
+    def setStartIp(self, string):
+        self.startIp = ipaddress.ip_address(string)
 
-    udp.sendto(pkt.RequestPacket(), ("172.17.150.27", 1813))
+    def setServer(self, string):
+        self.server = ipaddress.ip_address(string)
+    
+    def getServerStr(self):
+        return str(self.server)
 
-async def send(udp, pkt, n, semaphore: asyncio.Semaphore):
+    def getNextIpStr(self, n):
+        return str(self.startIp + n)
+
+    def setPacket(self):
+        server = self.getServerStr()
+        radclient = Client(server=server, secret=self.secret, dict=Dictionary("dictionary"))
+        self.pkt = radclient.CreateAcctPacket(code=pyrad.packet.AccountingRequest)
+
+def sendAccountingPkt(udp, Config, n, type):
+    Config.pkt["Acct-Status-Type"] = type
+    Config.pkt["User-Name"] = Config.usernameBase + str(n)
+    Config.pkt["Calling-Station-Id"] = Config.msisdnBase + str(n)
+    Config.pkt["Framed-IP-Address"] = Config.getNextIpStr(n)
+    Config.pkt["Acct-Session-Id"]=str(n)
+    server = Config.getServerStr()
+
+    udp.sendto(Config.pkt.RequestPacket(), (server, 1813))
+
+async def send(udp, Config, n, semaphore: asyncio.Semaphore):
     async with semaphore:
-        sendAccountingPkt(udp, pkt, n, "Start")
+        sendAccountingPkt(udp, Config, n, "Start")
         await asyncio.sleep(1)
-        sendAccountingPkt(udp, pkt, n, "Stop")
+        sendAccountingPkt(udp, Config, n, "Stop")
 
-async def async_main(server, secret, n, loop):
-    count = 0
-    max = n
-    s = asyncio.Semaphore(value=n)
+async def async_main(Config):
+    count = Config.count
+    server = Config.server
+    secret = Config.secret
+    start = 0
+    max = count
 
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
-    radclient=Client(server=server, secret=six.b(secret), dict=Dictionary("dictionary"))
-    pkt = radclient.CreateAcctPacket(code=pyrad.packet.AccountingRequest)
+    s = asyncio.Semaphore(value=count)
 
     while True:
-        cors = [send(udp, pkt, i, semaphore=s) for i in range(count,max)]
+        cors = [send(udp, Config, i, semaphore=s) for i in range(start,max)]
         await asyncio.gather(*cors)
-        if not loop:
-            count+=n
-            max+=n
+        if not Config.loop:
+            count+=count
+            max+=count
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Send RADIUS accouting packets')
     parser.add_argument("-c", "--count", type=int, default=10)
     parser.add_argument("-s", "--server", type=str, default="127.0.0.1")
     parser.add_argument("-p", "--secret", type=str, default="secret")
+    parser.add_argument("-sip", "--start", type=str, default="10.0.0.1")
+
     parser.add_argument("-l", "--loop", action="store_true")
     args = parser.parse_args()
+
+    cnf = Config()
+    cnf.count = args.count
+    cnf.loop = args.loop
+    cnf.setServer(args.server)
+    cnf.setSecret(args.secret)
+    cnf.setStartIp(args.start)
+    cnf.setPacket()
 
     print("target:"+args.server+", pps: "+str(args.count*2))
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(async_main(args.server, args.secret, args.count, args.loop))
+    loop.run_until_complete(async_main(cnf))
     loop.close()
